d870a9fa82ce3aee6c2590d3c0529f0caf3962a0
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-23 19:48:42 +0530

    add portoflio section + glass effect

--- Projects/portfolio/src/App.tsx
+++ Projects/portfolio/src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import Portfolio from './components/Portfolio/Portfolio';
 import './App.css';
 
 function App() {
   return (
-    <div className="hero">
-      <div className="hero-content">
-        <h1 className="hero-title">
-          Hi there, I'm<br />
-          <span className="hero-name">Snigdha!</span>
-        </h1>
-        <div className="hero-description">
-          <p>I AM A DEVELOPER BASED<br />IN LONDON. I have a passion for</p>
-          <ul className="hero-interests">
-            <li>+ global politics,</li>
-            <li>+ technology,</li>
-            <li>+ ethics,</li>
-            <li>+ photography</li>
-          </ul>
+    <div className="App">
+      {/* Your existing hero section */}
+      <div className="hero">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            Hi there, I'm<br />
+            <span className="hero-name">Snigdha!</span>
+          </h1>
+          <div className="hero-description">
+            <p>I AM A DEVELOPER BASED<br />IN LONDON. I have a passion for</p>
+            <ul className="hero-interests">
+              <li>+ global politics,</li>
+              <li>+ technology,</li>
+              <li>+ ethics,</li>
+              <li>+ photography</li>
+            </ul>
+          </div>
         </div>
+        <div className="hero-timestamp">01/02/2013 04:00 PM</div>
       </div>
-      <div className="hero-timestamp">01/02/2013 04:00 PM</div>
+      
+      {/* New portfolio section with glass effects */}
+      <Portfolio />
     </div>
   );
 }
